b5174e022dd62ab9dc4c2212cc2ea585f14fbd79
erhemdiputra <erhem.diputra@tokopedia.com>
2019-06-20 21:53:14 +0700

    fix dfs iterative , add 2 pointer technique

--- 000-my-cheatsheet/dfs-iterative/main.cpp
+++ 000-my-cheatsheet/dfs-iterative/main.cpp
@@ -1,62 +1,63 @@
-// #include <bits/stdc++.h>
+#include <bits/stdc++.h>
 
-// using namespace std;
+using namespace std;
 
-// vector<int> G[30005];
-// bool visited[30005];
-// int n;
+vector<int> G[30005];
+bool visited[30005];
+int n;
 
-// struct rec {
-//     int vertex;
-//     int step;
-// };
+struct rec {
+    int vertex;
+    int step;
+};
 
-// stack<rec> S;
+stack<rec> S;
 
-// rec make_rec(int vertex, int step) {
-//     rec result;
-//     result.vertex = vertex;
-//     result.step = step;
-//     return result;
-// }
+rec make_rec(int vertex, int step) {
+    rec result;
+    result.vertex = vertex;
+    result.step = step;
+    return result;
+}
 
-// int dfs(int start, int target) {
-//     rec v = make_rec(start, 1);
-//     S.push(v);
-//     visited[start] = true;
+int dfs(int start, int target) {
+    rec v = make_rec(start, 1);
+    S.push(v);
+    visited[start] = true;
 
-//     while(!S.empty()) {
-//         v = S.top();
-//         S.pop();
+    while(!S.empty()) {
+        v = S.top();
+        S.pop();
 
-//         if(v.vertex == target) return v.step;
-//         else {
-//             for(int i = 0; i < G[start].size(); i++) {
-//                 if(!visited[G[start][i]]) {
-//                     rec next = make_rec(G[start][i], v.step + 1);
-//                     S.push(next);
-//                     visited[G[start][i]] = true;
-//                 }
-//             }
-//         }
-//     }
-// }
+        if(v.vertex == target) return v.step;
+        else {
+            for(int i = 0; i < G[v.vertex].size(); i++) {
+                if(!visited[G[v.vertex][i]]) {
+                    rec next = make_rec(G[v.vertex][i], v.step + 1);
+                    S.push(next);
+                    visited[G[v.vertex][i]] = true;
+                }
+            }
+        }
+    }
 
-// int main() {
-//     int target, val;
-//     cin>>n>>target;
+    return -1;
+}
 
-//     for(int i = 1; i < n; i++) {
-//         cin>>val;
-//         G[i].push_back(i+val);
-//     }
+int main() {
+    int target, val;
+    cin>>n>>target;
 
-//     int found =  dfs(1, target);
-//     cout<<"FOUND = "<<found<<endl;
+    for(int i = 1; i < n; i++) {
+        cin>>val;
+        G[i].push_back(i+val);
+    }
 
-//     if(found == -1) {
-//         cout<<"NO"<<endl;
-//     } else {
-//         cout<<"YES"<<endl;
-//     }
-// }
+    int found =  dfs(1, target);
+
+    if(found == -1) {
+        cout<<"NO"<<endl;
+    } else {
+        cout<<"YES"<<endl;
+    }
+}
